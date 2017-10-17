2e5a80ca9c460969bcf73bd8683d1b2a8013b1bd
Ricardo Hernández <ricardohs35@gmail.com>
2017-10-16 23:34:28 -0500

    :heavy_plus_sign: Configure CORS

--- config/app.php
+++ config/app.php
@@ -168,6 +168,8 @@ return [
          * Package Service Providers...
          */
 
+        Barryvdh\Cors\ServiceProvider::class,
+
         /*
          * Application Service Providers...
          */
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/api', function () {
+    return [
+        'foo' => 'bar'
+    ];
 });
